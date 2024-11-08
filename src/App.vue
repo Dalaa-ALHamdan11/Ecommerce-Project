d79727531900b99c73c158e2b772d76a5c86f467
Dalaa-ALHamdan11 <dlalhmdan705@gmail.com>
2024-11-08 02:23:10 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+
+  <div>
+    <Header/>
+    <RouterView />
+  </div>
+
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+import Header from './components/Header.vue';
+</script>
+
+<style>
+.container {
+  max-width: calc(1440px - 170px);
+    width: 100%;
+    margin: auto;
+}
+
+.btn {
+  background: transparent;
+  border: none;
+  transition: .3s;
+  cursor: pointer;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+img {
+  width: 100%;
+}
+
+span .icon {
+  display: flex;
+  align-items: center;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <Banner />
+    <CategoryProducts :tabs="womentabs" :title="womentitle" :products="womenProducts" />
+    <CategoryProducts :tabs="womentabs" :title="mentitle" :products="womenProducts" />
+    <Feauterd />
+    <Subscribe />
+    <Blogs />
+    <Footer />
+  </div>
+</template>
+
+
+<script setup>
+import { ref } from 'vue'
+import Banner from '@/components/Banner.vue';
+import CategoryProducts from '@/components/CategoryProducts.vue';
+import Feauterd from '@/components/Feauterd.vue';
+import Subscribe from '@/components/Subscribe.vue';
+import Blogs from '@/components/Blogs.vue';
+import Footer from '@/components/Footer.vue';
+const womentabs = ref([
+  "NEW ARRIVALS",
+  "SPECIALS",
+  "BESTSELLERS",
+  "MOST VIEWED",
+  "FEATURED PRODUCTS",
+]);
+
+const womenProducts = ref([
+  {
+    img: "../assets/images/p1.jpg",
+    sale: false,
+    name: `Women's tracksuit Q109`,
+    price: "$ 38.00",
+    rating: "../assets/images/rating.png",
+  },
+  {
+    img: "../assets/images/p2.png",
+    sale: true,
+    name: `Women's tracksuit Q109`,
+    price: "$ 38.00",
+    rating: "../assets/images/rating.png",
+  },
+  {
+    img: "../assets/images/p3.png",
+    sale: false,
+    name: `Women's tracksuit Q109`,
+    price: "$ 38.00",
+    rating: "../assets/images/rating.png",
+  },
+  {
+    img: "../assets/images/p2.png",
+    sale: false,
+    name: `Women's tracksuit Q109`,
+    price: "$ 38.00",
+    rating: "../assets/images/rating.png",
+  },
+  {
+    img: "../assets/images/p3.png",
+    sale: false,
+    name: `Women's tracksuit Q109`,
+    price: "$ 38.00",
+    rating: "../assets/images/rating.png",
+  },
+  {
+    img: "../assets/images/p1.jpg",
+    sale: false,
+    name: `Women's tracksuit Q109`,
+    price: "$ 38.00",
+    rating: "../assets/images/rating.png",
+  },
+  {
+    img: "../assets/images/p3.png",
+    sale: false,
+    name: `Women's tracksuit Q109`,
+    price: "$ 38.00",
+    rating: "../assets/images/rating.png",
+  },
+  {
+    img: "../assets/images/p2.png",
+    sale: false,
+    name: `Women's tracksuit Q109`,
+    price: "$ 38.00",
+    rating: "../assets/images/rating.png",
+  },
+]);
+const womentitle = ref("Womens");
+const mentitle = ref("Men");
+</script>
